6d684b9b4d2197227a4b7d2e874d16b1331e0928
Kamil Duda <kamil.duda@ing.com>
2018-08-29 15:57:21 +0200

    Add Consul module

---
+++ consul-discovery/src/main/java/com/kduda/consul/discovery/ConsulDiscoveryApplication.java
@@ -0,0 +1,11 @@
+package com.kduda.consul.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulDiscoveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulDiscoveryApplication.class, args);
+    }
+}
